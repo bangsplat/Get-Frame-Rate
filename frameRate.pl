a34cba6e076cd31994f25c5d90ce377b93406b2c
Bangsplat <theron@dankerton.net>
2015-12-15 13:07:16 -0800

    Adding MediaInfo CLI check

--- frameRate.pl
+++ frameRate.pl
@@ -3,14 +3,16 @@ use strict;	# Enforce some good programming rules
 
 #
 # frameRate.pl
-# version 0.1
+# version 0.2
 # 
 # created 2015-12-15
 # modified 2015-12-15
 #
 # use command line MediaInfo to determine video frame rate of media file
 # 
-# assumes MeidaInfo CLI is installed
+# requires a filename passed as first command line parameter
+# requires MediaInfo CLI to be installed
+# 	(checks for both of these)
 # 
 
 # input is first ARGV[]
@@ -18,6 +20,10 @@ my $input_file = $ARGV[0];
 
 # make sure an input file was specified
 if ( $input_file eq undef ) { die "frameRate.pl <filename>\n\nPlease specify an input file\n"; }
+
+# check to see if MediaInfo CLI is installed
+my $MediaInfoVersion = `mediainfo --Version`;
+if ( ! ( $MediaInfoVersion =~ /MediaInfoLib/ ) ) { die "MediaInfo Command Line not detected\n"; }
 
 # run MediaInfo, parse out the <Frame_rate> tag from the first Video track and return it
 my $MediaInfoOutput = `mediainfo --Output=XML \"$input_file\"`;
